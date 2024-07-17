ef1916c79f143066f24a919a5d8a34370429c860
alexandr reznic <reznic.a@gmail.com>
2024-07-17 13:22:53 +0300

    init

---
+++ src/main/kotlin/com/reznicsoftware/buildtime/BuildTimeConfigExtension.kt
@@ -0,0 +1,22 @@
+package com.reznicsoftware.buildtime
+
+import org.gradle.api.Action
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Nested
+
+abstract class BuildTimeInfoExtension {
+    abstract val androidStudioVersion: Property<String>
+    abstract val serverURL: Property<String>
+}
+
+abstract class BuildTimeOptionsExtension {
+
+    abstract val sendData: Property<Boolean?>
+
+    @Nested
+    abstract fun getInfo(): BuildTimeInfoExtension?
+
+    fun info(action: Action<in BuildTimeInfoExtension>) {
+        action.execute(getInfo())
+    }
+}
