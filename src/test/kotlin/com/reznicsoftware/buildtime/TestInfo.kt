aef0b52afce8e01d09db420ec2a55095b6875fce
Alexandru Reznic <reznic.a@gmail.com>
2024-08-04 16:51:45 +0300

    0.2.0

--- src/test/kotlin/com/reznicsoftware/buildtime/TestInfo.kt
+++ src/test/kotlin/com/reznicsoftware/buildtime/TestInfo.kt
@@ -1,14 +1,13 @@
+package com.reznicsoftware.buildtime
+
 import com.reznicsoftware.buildtime.dto.InputResultDTO
-import com.reznicsoftware.buildtime.json
 import com.reznicsoftware.buildtime.utils.SysInfo
 import kotlinx.serialization.encodeToString
 import kotlin.test.Test
-
 
 class TestInfo {
     @Test
     fun testInfo() {
         println(json.encodeToString<InputResultDTO>(SysInfo.getAll()))
-
     }
 }
